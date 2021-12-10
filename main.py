46ffdcb8a1dda2642f387f8d0dca0c46186c2861
Devin <devin@hiitsdevin.dev>
2021-12-09 21:12:06 -0800

    HTTP Cats said hi.

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ async def on_ready():
 async def hello(ctx):
     await ctx.reply("howdy", mention = True)
 
+@yiski.command()
+async def httpcat(ctx, httpcode: str):
+    await ctx.reply("[cat](https://http.cat/" + httpcode + ")")
+
 yiski.run("Fkf_adLmBvlI_IGRjzF8VN-it6eC7At-NLJadYgOhu3OaQ4Q6pQOaZpqduuSs_Dn")
