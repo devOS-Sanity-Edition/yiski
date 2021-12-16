0533245c0a9cc03ac39e83fa3f50d3c3399dcc34
Devin <devin@hiitsdevin.dev>
2021-12-16 12:43:55 -0800

    `yiskiDiscord` -> `yD` and `yiskiRevolt` -> `yR`

--- mainDiscord.py
+++ mainDiscord.py
@@ -13,7 +13,7 @@ ventChannel = yiskiConf["ventChannelDiscordID"]
 
 intents = discord.Intents().all()
 
-yiskiDiscord = commands.Bot(command_prefix=commandPrefix, intents=intents, activity=discord.Activity(type=discord.ActivityType.listening, name="my bones snap"), status=discord.Status.dnd, help_command=None)
+yD = commands.Bot(command_prefix=commandPrefix, intents=intents, activity=discord.Activity(type=discord.ActivityType.listening, name="my bones snap"), status=discord.Status.dnd, help_command=None)
 
 def embedCreator(title, desc, color):
     embed = discord.Embed(
@@ -23,12 +23,12 @@ def embedCreator(title, desc, color):
     )
     return embed
 
-@yiskiDiscord.event
+@yD.event
 async def on_ready():
     print("howdy from Discord")
 
 # Error Handling
-@yiskiDiscord.event
+@yD.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send(embed=embedCreator("Unknown Command", f"The command `{ctx.message.content.split(' ')[0]}` is not found! Use `{commandPrefix}help` to list all commands!", 0xbf1300))
@@ -36,43 +36,43 @@ async def on_command_error(ctx, error):
     else:
         await ctx.send(embed=embedCreator("Error", f"Unexpected Error: `{error}`", 0xff0000))
 
-@yiskiDiscord.command(aliases=["https://www.youtube.com/watch?v=U9t-slLl30E"])
+@yD.command(aliases=["https://www.youtube.com/watch?v=U9t-slLl30E"])
 async def stop(ctx):
     await ctx.send(embed=embedCreator("Stopping", "Shutting Down Yiski", 0xFF0000))
-    await yiskiDiscord.close()
+    await yD.close()
 # Reloads all commands
-@yiskiDiscord.command(aliases=["relaod"])  # this alias is here seriously just because i was tired of speed type misspelling it
+@yD.command(aliases=["relaod"])  # this alias is here seriously just because i was tired of speed type misspelling it
 async def reload(ctx, extension = None):
     if not extension:
         try:
             for filename in os.listdir('./discordCommands/'):
                 if filename.endswith('.py'):
-                    yiskiDiscord.unload_extension(f'discordCommands.{filename[:-3]}')
-                    yiskiDiscord.load_extension(f'discordCommands.{filename[:-3]}')
+                    yD.unload_extension(f'discordCommands.{filename[:-3]}')
+                    yD.load_extension(f'discordCommands.{filename[:-3]}')
             await ctx.send(embed=embedCreator("Reloaded", "All cogs reloaded", 0x00ad10))
         except Exception as e:
             await ctx.send(embed=embedCreator("Error Reloading", f"`{e}`", 0xbf1300))
     else:
         try:
-            yiskiDiscord.unload_extension(f'discordCommands.{extension}')
-            yiskiDiscord.load_extension(f'discordCommands.{extension}')
+            yD.unload_extension(f'discordCommands.{extension}')
+            yD.load_extension(f'discordCommands.{extension}')
             await ctx.send(embed=embedCreator(f"Reloaded", f"{extension} has been reloaded.", 0x00ad10))
         except Exception as e:
             await ctx.send(embed=embedCreator(f"Error reloading {extension}", f"{e}", 0xbf1300))
 
-@yiskiDiscord.command()
+@yD.command()
 async def load(ctx, extension):
-    yiskiDiscord.load_extension(f'discordCommands.{extension}')
+    yD.load_extension(f'discordCommands.{extension}')
     await ctx.send(embed=embedCreator(f"Loaded", f"{extension} has been loaded.", 0x00ad10))
 
-@yiskiDiscord.command()
+@yD.command()
 async def unload(ctx, extension):
-    yiskiDiscord.unload_extension(f'discordCommands.{extension}')
+    yD.unload_extension(f'discordCommands.{extension}')
     await ctx.send(embed=embedCreator(f"Unloaded", f"{extension} has been unloaded.", 0x00ad10))
 
 # load cogs on startup
 for filename in sorted(os.listdir('./discordCommands/')):
     if filename.endswith('.py'):
-        yiskiDiscord.load_extension(f'discordCommands.{filename[:-3]}')
+        yD.load_extension(f'discordCommands.{filename[:-3]}')
 
-yiskiDiscord.run(yiskiConf["yiskiDiscordBotToken"])
+yD.run(yiskiConf["yiskiDiscordBotToken"])
--- mainRevolt.py
+++ mainRevolt.py
@@ -10,22 +10,22 @@ with open("config.json5", "r") as yiskiConfig:
 githubToken = yiskiConf["githubToken"]
 commandPrefix = yiskiConf["yiskiBotPrefix"]
 
-yiskiRevolt = defectio.ext.commands.Bot(command_prefix=commandPrefix, help_command=None)
+yR = defectio.ext.commands.Bot(command_prefix=commandPrefix, help_command=None)
 logger.add("logs/yiskiRevolt_{time}.log", format="[Yiski Revolt][{time:HH:mm:ss}][{level}] {message}", enqueue=True, colorize=True)
 
 # load cogs on startup
 for filename in sorted(os.listdir('./revoltCommands/')):
     if filename.endswith('.py'):
         logger.debug("Loading Cog:" + f'revoltCommands.{filename[:-3]}')
-        yiskiRevolt.load_extension(f'revoltCommands.{filename[:-3]}')
+        yR.load_extension(f'revoltCommands.{filename[:-3]}')
 
 
-@yiskiRevolt.event
+@yR.event
 async def on_ready():
     logger.debug("Bots started on Revolt end.")
 
 
-@yiskiRevolt.event
+@yR.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.reply(
@@ -34,15 +34,15 @@ async def on_command_error(ctx, error):
 
 
 # Reloads all commands
-@yiskiRevolt.command(aliases=["relaod"])  # this is here seriously just because i was tired of speed type misspelling it
+@yR.command(aliases=["relaod"])  # this is here seriously just because i was tired of speed type misspelling it
 async def reload(ctx, extension = None): # ftr, `extension: None` doesn't work, but `extension = None` does, so that's nice lol.
     logger.debug("Attempt to reload cogs have started")
     if not extension:
         try:
             for filename in os.listdir('./revoltCommands/'):
                 if filename.endswith('.py'):
-                    yiskiRevolt.unload_extension(f'revoltCommands.{filename[:-3]}')
-                    yiskiRevolt.load_extension(f'revoltCommands.{filename[:-3]}')
+                    yR.unload_extension(f'revoltCommands.{filename[:-3]}')
+                    yR.load_extension(f'revoltCommands.{filename[:-3]}')
             await ctx.reply("# Reloaded\n" + "All cogs have been reloaded.", mention=True)
             logger.debug("Attempted reload of cogs successful.")
         except Exception as e:
@@ -50,24 +50,24 @@ async def reload(ctx, extension = None): # ftr, `extension: None` doesn't work, 
             logger.error(f"Attempted reload of cogs failed, error {e}")
     else:
         try:
-            yiskiRevolt.unload_extension(f'revoltCommands.{extension}')
-            yiskiRevolt.load_extension(f'revoltCommands.{extension}')
+            yR.unload_extension(f'revoltCommands.{extension}')
+            yR.load_extension(f'revoltCommands.{extension}')
             await ctx.reply("# Reloaded\n" + f"{extension} has been reloaded.", mention=True)
             logger.debug(f"Attempted reload of {extension} cog successful.")
         except Exception as e:
             await ctx.reply(f"# {extension} Reload Failed.\n" + f"Error {e}", mention=True)
             logger.error(f"Attempted reload of {extension} cog failed, error {e}")
 
-@yiskiRevolt.command()
+@yR.command()
 async def load(ctx, extension):
-    yiskiRevolt.load_extension(f'revoltCommands.{extension}')
+    yR.load_extension(f'revoltCommands.{extension}')
     logger.debug(f"Attempted load of {extension}")
     await ctx.reply(f"Loaded {extension}", mention=True)
 
-@yiskiRevolt.command()
+@yR.command()
 async def unload(ctx, extension):
-    yiskiRevolt.unload_extension(f'revoltCommands.{extension}')
+    yR.unload_extension(f'revoltCommands.{extension}')
     logger.debug(f"Attempted unload of {extension}")
     await ctx.reply(f"Unloaded {extension}", mention=True)
 
-yiskiRevolt.run(yiskiConf["yiskiBotToken"])
+yR.run(yiskiConf["yiskiBotToken"])
