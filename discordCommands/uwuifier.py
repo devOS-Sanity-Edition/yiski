d95c68e61f20ae03abf6c376a1c3c7a3930ef43e
Devin <devin@hiitsdevin.dev>
2021-12-15 16:02:19 -0800

    UWU - discord

---
+++ discordCommands/uwuifier.py
@@ -0,0 +1,20 @@
+import uwuify
+from discord.ext import commands
+from mainDiscord import embedCreator
+
+
+class FunDiscord(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def uwu(self, ctx, *, uwumessage = None):
+        if not uwumessage:
+            await ctx.send(embed=embedCreator("uwuifier", "pwovide some fwucking text You Fucking Dingus.", 0x00ff00))
+        else:
+            hell = uwuify.uwu(f"{uwumessage}")
+            await ctx.send(embed=embedCreator("uwuifier", f"{hell}", 0x00ff00))
+
+
+def setup(client):
+    client.add_cog(FunDiscord(client))
