cbb61806cf444028009453c45fa25d688cfb8890
Devin <devin@hiitsdevin.dev>
2022-02-10 15:30:05 -0800

    Added `setup.py` for `pip install .`

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(name='Yiski',
+                 version='0.1.0',
+                 license="MIT",
+                 packages=find_packages(),
+                 description="A bot to replace the original idea of devOS Core for the devOS: Sanity Edition server.",
+                 author="HiItsDevin_",
+                 url="https://github.com/HiItsDevin/Yiski",
+                 python_requires=">=3.9"
+)
