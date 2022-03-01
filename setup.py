a22cce1f134afd0ddd7ed1c8d91a0d7e57072eb4
Devin <asoji-git@hiitsdevin.mozmail.com>
2022-02-28 19:56:33 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from setuptools import find_packages
 
 setuptools.setup(name='Yiski',
-                 version='0.1.0',
+                 version='0.1.1',
                  license="MIT",
                  packages=find_packages(),
                  description="A bot to replace the original idea of devOS Core for the devOS: Sanity Edition server.",
