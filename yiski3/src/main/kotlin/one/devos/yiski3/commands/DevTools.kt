7090243ccc541049016266ccc6cb3024047247a5
Hunter <hunter@devos.one>
2024-07-25 06:45:42 -0400

    🦝😺 Refactored devtools so image shows

--- yiski3/src/main/kotlin/one/devos/yiski3/commands/DevTools.kt
+++ yiski3/src/main/kotlin/one/devos/yiski3/commands/DevTools.kt
@@ -1,9 +1,14 @@
 package one.devos.yiski3.commands
 
 import com.akuleshov7.ktoml.file.TomlFileReader
+import dev.minn.jda.ktx.coroutines.await
+import dev.minn.jda.ktx.messages.Embed
 import kotlinx.serialization.serializer
+import net.dv8tion.jda.api.utils.FileUpload
 import one.devos.yiski.common.annotations.YiskiModule
 import one.devos.yiski.common.entrypoints.ConfigSetupEntrypoint
+import one.devos.yiski.common.utils.EmbedHelpers
+import one.devos.yiski3.Yiski3
 import one.devos.yiski3.data.DevToolsData
 import one.devos.yiski3.logger
 import xyz.artrinix.aviation.command.slash.SlashContext
@@ -16,16 +21,22 @@ class DevTools : Scaffold {
     @SlashCommand(name = "devtools", description = "How to turn on Discord Dev Tools on stable branch")
     suspend fun devtools(ctx: SlashContext) {
         val devToolsToml = DevToolsToml.read()
+        val devToolsImage = EmbedHelpers.imagesPath(Yiski3.config.images.inlineStaticImagesTables.devtools)
 
-        ctx.sendEmbed {
-            setTitle(devToolsToml.title.embed)
-            setDescription(devToolsToml.text.embed)
-            addField(devToolsToml.title.windows, devToolsToml.text.windows, false)
-            addField(devToolsToml.title.macOS, devToolsToml.text.macOS, false)
-            addField(devToolsToml.title.linux, devToolsToml.text.linux, false)
-            addField(devToolsToml.title.result, devToolsToml.text.result, false)
-            addField(devToolsToml.title.source, devToolsToml.text.source, false)
-        }
+        ctx.interaction.deferReply()
+            .setFiles(FileUpload.fromData(devToolsImage, Yiski3.config.images.inlineStaticImagesTables.devtoolsfile))
+            .setEmbeds(Embed {
+                title = devToolsToml.title.embed
+                description = devToolsToml.text.embed
+                color = EmbedHelpers.infoColor()
+                field(devToolsToml.title.windows, devToolsToml.text.windows, false)
+                field(devToolsToml.title.macOS, devToolsToml.text.macOS, false)
+                field(devToolsToml.title.linux, devToolsToml.text.linux, false)
+                field(devToolsToml.title.result, devToolsToml.text.result, false)
+                field(devToolsToml.title.source, devToolsToml.text.source, false)
+                image = "attachment://${Yiski3.config.images.inlineStaticImagesTables.devtoolsfile}"
+            })
+            .await()
 
     }
 }
