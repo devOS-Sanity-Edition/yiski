ea874a2c4cea60d18934383aa1aab9e4abe3fb2c
Asoji <asoji-git@hiitsdevin.mozmail.com>
2024-08-02 19:07:15 -0700

    ✏️ adjusted yiski version constant

--- yiski-common/src/main/kotlin/one/devos/yiski/common/YiskiConstants.kt
+++ yiski-common/src/main/kotlin/one/devos/yiski/common/YiskiConstants.kt
@@ -7,8 +7,8 @@ import xyz.artrinix.aviation.Aviation
 
 object YiskiConstants {
     // I've been told to set this as a getter bc apparently it's good practice. im not gonna question it lmao.
-    val version: String get() = YiskiConstants::class.java.`package`.implementationVersion ?: "DEV"
-    val aviationVersion: String get() = Aviation::class.java.`package`.implementationVersion ?: "CANNOT GET AVIATION VERSION. IF YOU'RE SEEING THIS... *WELL FUCK*!"
+    val version: String get() = YiskiConstants::class.java.`package`.implementationVersion ?: "Development"
+    val aviationVersion: String get() = Aviation::class.java.`package`.implementationVersion ?: "Aviation Version isn't available, go ask Storm."
     val jdaVersion: String get() = JDAInfo.VERSION
     val config = Config.loadConfig()
     val moduleLoader = ModuleLoader()
