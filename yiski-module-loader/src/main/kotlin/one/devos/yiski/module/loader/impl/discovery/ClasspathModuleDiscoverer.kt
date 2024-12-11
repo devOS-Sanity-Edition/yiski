1b071e229c7c73166b33b85b3c22eee224c19547
Storm Anderson <storm@artrinix.xyz>
2024-12-11 16:31:44 +1300

    Revert ":wastebasket: 'constructor URL(String!, String!, Int, String!)' is deprecated. Deprecated in Java"
    This reverts commit 57272f016503b2d08f6178c4ab41c4e06e4ee3e5.

--- yiski-module-loader/src/main/kotlin/one/devos/yiski/module/loader/impl/discovery/ClasspathModuleDiscoverer.kt
+++ yiski-module-loader/src/main/kotlin/one/devos/yiski/module/loader/impl/discovery/ClasspathModuleDiscoverer.kt
@@ -4,7 +4,6 @@ import one.devos.yiski.module.loader.api.discovery.ModuleDiscoverer
 import one.devos.yiski.module.loader.impl.ModuleLoader
 import one.devos.yiski.module.loader.impl.logger
 import one.devos.yiski.module.metadata.ModuleMetadata
-import java.net.URI
 import java.net.URL
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -31,7 +30,7 @@ class ClasspathModuleDiscoverer : ModuleDiscoverer {
     private fun fetchCodeSource(url: URL, path: String): Path {
         val urlPath = url.path
         if (urlPath.endsWith(path))
-            return Paths.get(URI(url.protocol, url.host, url.port.toString(), urlPath.substring(0, urlPath.length - path.length)))
+            return Paths.get(URL(url.protocol, url.host, url.port, urlPath.substring(0, urlPath.length - path.length)).toURI())
 
         throw IllegalStateException("Failed to fetch code source for file \"$path\" inside \"$url\"!")
     }
