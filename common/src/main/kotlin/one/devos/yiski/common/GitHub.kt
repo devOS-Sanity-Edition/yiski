c01b3f5acc8c669c21fa35f93153423b621dc202
Storm Anderson <storm@artrinix.xyz>
2026-02-04 02:27:08 +1300

    :art: Abstract out the Http client for reusing.

--- common/src/main/kotlin/one/devos/yiski/common/GitHub.kt
+++ common/src/main/kotlin/one/devos/yiski/common/GitHub.kt
@@ -4,24 +4,17 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.expectSuccess
 import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.*
 import one.devos.yiski.common.types.github.GitHubRepositoryResponse
 
 object GitHub {
-    private val client = HttpClient(CIO) {
-        expectSuccess = true
-        install(ContentNegotiation) {
-            json(Json {
-                ignoreUnknownKeys = true
-            })
-        }
-    }
-
-
     suspend fun getRepository(): GitHubRepositoryResponse {
-        val response = client.get("https://api.github.com/repos/devOS-Sanity-Edition/yiski")
+        val response = Http.client.get("https://api.github.com/repos/devOS-Sanity-Edition/yiski") {
+            expectSuccess = true
+        }
 
         return response.body<GitHubRepositoryResponse>()
     }
---
+++ common/src/main/kotlin/one/devos/yiski/common/Http.kt
@@ -0,0 +1,17 @@
+package one.devos.yiski.common
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object Http {
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+}
